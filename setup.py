77c9567514bf5088ee3350200e664e18e2de1c68
Alyssa Vance <alyssamvance@gmail.com>
2025-03-26 15:28:05 -0600

    Add init.py files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="flash-attn-triton",
-    version="0.1.0",
+    version="0.1.1",
     author="Alyssa Vance",
     author_email="alyssamvance@gmail.com",
     description="Triton-based backend for Flash Attention 2",
